905469efd726c458f00c35206cfd22fd807c7f71
ChillerDragon <ChillerDragon@gmail.com>
2019-07-12 15:05:27 +0200

    Make sure the logs folder exist

--- gdb.sh
+++ gdb.sh
@@ -7,7 +7,21 @@ then
     exit
 fi
 
+if [ ! -d /home/$USER/git/TeeworldsLogs ]
+then
+    echo "Error: log path not found /home/$USER/git/TeeworldsLogs"
+    echo "make sure to create this folder"
+    exit
+fi
+
 srv=$(cat srv.txt)
+
+if [ ! -f "$srv" ]
+then
+    echo "Error: server binary '$srv' not found!"
+    echo "make sure the binary and your current path match"
+    exit
+fi
 
 gdb --args ./${srv}_srv_d "logfile /home/$USER/git/TeeworldsLogs/$srv/logs/${srv}_$(date +%F_%H-%M-%S).log"
 
--- start.sh
+++ start.sh
@@ -7,7 +7,21 @@ then
     exit
 fi
 
+if [ ! -d /home/$USER/git/TeeworldsLogs ]
+then
+    echo "Error: log path not found /home/$USER/git/TeeworldsLogs"
+    echo "make sure to create this folder"
+    exit
+fi
+
 srv=$(cat srv.txt)
 
-nohup ./${srv}_srv_d > ../git/TeeworldsLogs/$srv/logs/${srv}_$(date +%F_%H-%M-%S).log 2>&1 &
+if [ ! -f "$srv" ]
+then
+    echo "Error: server binary '$srv' not found!"
+    echo "make sure the binary and your current path match"
+    exit
+fi
+
+nohup ./${srv}_srv_d > /home/$USER/git/TeeworldsLogs/$srv/logs/${srv}_$(date +%F_%H-%M-%S).log 2>&1 &
 
