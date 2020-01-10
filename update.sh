65d7d5e918a467f68130c3eefc8826a623a34029
ChillerDragon <ChillerDragon@gmail.com>
2020-01-10 15:41:46 +0100

    Create folderes in update script

--- update.sh
+++ update.sh
@@ -3,9 +3,11 @@ srv=fddrace
 gitpath=/home/$USER/git/F-DDrace
 cwd=$(pwd)
 
+mkdir -p maps || { echo "Error: creating dir maps/"; exit 1; }
+mkdir -p logs || { echo "Error: creating dir logs/"; exit 1; }
 cd $gitpath
 git pull
-mkdir -p build
+mkdir -p build || { echo "Error: creating dir build/"; exit 1; }
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
 make -j6
