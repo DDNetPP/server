49c91c44e289d498304958030c23605410e0c1ca
ChillerDragon <ChillerDragon@gmail.com>
2020-01-22 13:21:38 +0000

    Use config values for crashsave.sh

--- crashsave.sh
+++ crashsave.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-srv_name=fddrace
+if [ ! -f lib/lib.sh ]
+then
+    echo "Error: lib/lib.sh not found!"
+    echo "make sure you are in the root of the server repo"
+    exit 1
+fi
+
+source lib/lib.sh
+
+check_deps
+check_running
+get_sid
 
 while true;
 do
-    ts=$(date +%F_%H-%M-%S)
-    logfile=TeeworldsLogs/$srv_name/logs/${srv_name}_$ts
-    ./${srv_name}_srv "logfile $logfile;#sid:fddrace-BlmapChill"
-    ts=$(date +%F_%H-%M-%S)
+    logfile="$gitpath_log/TeeworldsLogs/$srv_name/logs/${srv_name}_$(date +%F_%H-%M-%S).log"
+    ts_start=$(date +%F_%H-%M-%S)
+    ./${srv}_srv_d "#sid:$server_id" > "$logfile"
+    ts_end=$(date +%F_%H-%M-%S)
     echo "+----------------------------------------+"
-    echo ""
     echo ""
     figlet crash
-    echo ""
+    date
     echo ""
     echo "+----------------------------------------+"
-    echo "echo 'crash or shutdown $ts'" >> crashes.txt
+    echo "echo crash or shutdown" >> crashes.txt
+    echo "echo start: $ts_start" >> crashes.txt
+    echo "echo crash: $ts_end" >> crashes.txt
+    echo "echo ------------------" >> crashes.txt
 done
 
