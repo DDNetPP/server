51fea71edd5df654fe2d213e31f4c7d48314a8a9
ChillerDragon <ChillerDragon@gmail.com>
2020-01-26 21:54:27 +0000

    Hide server id file to avoid accidental copying

--- lib/sid.sh
+++ lib/sid.sh
@@ -2,29 +2,31 @@
 
 # each server directory has its own server id
 # those are openssl generated base64 encoded
-# and they are generated once and then stored into lib/server_id.txt
+# and they are generated once and then stored into lib/.server_id
 # they are used to kill the process and make sure not to kill the wrong server
+
+SID_FILE=lib/.server_id
 
 function generate_sid() {
     server_id=$(openssl rand -hex 16)
-    echo $server_id > lib/server_id.txt
+    echo "$server_id" > "$SID_FILE"
     log "generated new server id '$server_id'"
 }
 
 function get_sid() {
-    if [ ! -f lib/server_id.txt ]
+    if [ ! -f "$SID_FILE" ]
     then
         generate_sid
         return
     fi
-    server_id=$(cat lib/server_id.txt)
+    server_id="$(cat "$SID_FILE")"
     if [ "$server_id" == "" ]
     then
         wrn "server id empty generating new one..."
         generate_sid
         return
     fi
-    len=${#server_id}
+    len="${#server_id}"
     if [ $((len)) -ne 32 ]
     then
         wrn "unexpected sid length found"
