3bb6917a96fc53fdcdc8488e322e66f7352dda29
ChillerDragon <ChillerDragon@gmail.com>
2020-01-28 18:06:16 +0000

    Unhardcode loop_gdb.sh

--- loop_gdb.sh
+++ loop_gdb.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ ! -f lib/lib.sh ]
+then
+    echo "Error: lib/lib.sh not found!"
+    echo "make sure you are in the root of the server repo"
+    exit 1
+fi
+
+source lib/lib.sh
+
 function del_file() {
     local file="$1"
     if [ -f "$file" ]
@@ -31,22 +40,25 @@ then
     exit
 fi
 
-srv_name=fddrace
+check_deps
+check_running
+get_sid
 
-logfile=TeeworldsLogs/$srv_name/logs/${srv_name}_$(date +%F_%H-%M-%S).log
 ts=$(date +%F_%H-%M-%S)
 echo "started script at $ts" > crashes.txt
 
 while true;
 do
+    ts=$(date +%F_%H-%M-%S)
+    logfile="$gitpath_log/$srv_name/logs/${srv_name}_$ts.log"
+    cache_logpath "$logfile"
     echo "============= server start $ts =============" >> raw_gdb.txt
     gdb -ex='set confirm off' \
         -ex='set pagination off' \
         -ex='set logging file raw_gdb.txt' \
         -ex='set logging on' \
         -ex=run -ex=bt -ex=quit --args \
-        ./${srv_name}_srv "logfile $logfile;#sid:fddrace-BlmapChill"
-    ts=$(date +%F_%H-%M-%S)
+        ./${srv}_srv_d "logfile $logfile;#sid:$server_id"
     # filter out the thread spam
     grep -v '^\[New Thread' raw_gdb.txt | grep -v '^\[Thread' > tmp_gdb.txt
     mv tmp_gdb.txt raw_gdb.txt
