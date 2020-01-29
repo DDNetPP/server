9c208980cbba9e3b924ff6b73da226d716551a37
ChillerDragon <ChillerDragon@gmail.com>
2020-01-29 00:42:05 +0000

    Made loop_gdb.sh editable without stopping it

--- loop_gdb.sh
+++ loop_gdb.sh
@@ -61,35 +61,12 @@ then
     exit 1
 fi
 
-
-ts=$(date +%F_%H-%M-%S)
+ts="$(date +%F_%H-%M-%S)"
 echo "started script at $ts" > crashes.txt
 
 while true;
 do
-    ts=$(date +%F_%H-%M-%S)
-    logfile="$gitpath_log/$srv_name/logs/${srv_name}_$ts.log"
-    cache_logpath "$logfile"
-    echo "============= server start $ts =============" >> raw_gdb.txt
-    gdb -ex='set confirm off' \
-        -ex='set pagination off' \
-        -ex='set logging file raw_gdb.txt' \
-        -ex='set logging on' \
-        -ex=run -ex=bt -ex=quit --args \
-        ./${srv}_srv_d "logfile $logfile;#sid:$server_id"
-    # filter out the thread spam
-    grep -v '^\[New Thread' raw_gdb.txt | grep -v '^\[Thread' > tmp_gdb.txt
-    mv tmp_gdb.txt raw_gdb.txt
-    cat raw_gdb.txt | ./lib/echo_pipe.sh >> bt.txt
-    cat raw_gdb.txt >> log_gdb.txt
-    rm raw_gdb.txt
-    echo "echo 'crash or shutdown $ts'" >> crashes.txt
-    ./cmake_update.sh 2>&1 > raw_build.txt
-    url="$(cstd raw_build.txt)"
-    echo "echo $url" > paste.txt
-    cat raw_build.txt | ./lib/echo_pipe.sh > build.txt
-    echo "git status - $(date)" | ./lib/echo_pipe.sh > status.txt
-    git status 2>&1 | ./lib/echo_pipe.sh >> status.txt
-    sleep 5
+    ./lib/include/gdb_loop.sh "$logroot" \
+        "$srv_name" "$srv" "$server_id"
 done
 
