ffcc5c54942783c8c1d5c1f9e7f9db9cb014c4a4
ChillerDragon <ChillerDragon@gmail.com>
2020-02-02 15:11:06 +0000

    Move logfile functions to own lib file

---
+++ lib/include/logs.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+function check_logpath() {
+    local logpath
+    if [ ! -d lib/ ] || [ ! -f lib/tmp/logpath.txt ]
+    then
+        return
+    fi
+    logpath="$(cat lib/tmp/logpath.txt)"
+    if [ "$logpath" == "" ]
+    then
+        return
+    fi
+    if [ ! -f "$logpath" ] && [ ! -f "$logpath.txt" ]
+    then
+        wrn "WARNING: did not find logfile:"
+        echo "$logpath"
+    fi
+}
+
+function cache_logpath() {
+    local logpath="$1"
+    if [ ! -d lib/ ]
+    then
+        return
+    fi
+    mkdir -p lib/tmp
+    if [ "$is_dumps_logpath" == "1" ]
+    then
+        if [ "$HOME" == "" ]
+        then
+            err "Error: \$HOME is not set"
+            exit 1
+        fi
+        dumpsdir="^$HOME/.teeworlds/dumps/"
+        if [[ ! "$logpath" =~ $dumpsdir ]]
+        then
+            logpath="$HOME/.teeworlds/dumps/$logpath"
+        fi
+    fi
+    echo "$logpath" > lib/tmp/logpath.txt
+}
+
+function show_latest_logs() {
+    logpath="$gitpath_log/$srv_name/logs/"
+    if [ ! -d $logpath ]
+    then
+        err "logpath not found '$logpath'"
+        exit 1
+    fi
+    latest_log="$(ls "$logpath" | tail -n1)"
+    latest_log="$logpath$latest_log"
+    if [ ! -f $latest_log ]
+    then
+        wrn "there are no logfiles yet."
+        exit 1
+    fi
+    num_logs="$(ls "$logpath" | wc -l)"
+    log "showing latest logfile out of $num_logs logs:"
+    echo "$latest_log"
+    less "$latest_log"
+}
+
+function show_logs() {
+    log "do you want to show logs? [y/N]"
+    read -r -n 1 yn
+    echo ""
+    if [[ ! "$yn" =~ [yY] ]]
+    then
+        return
+    fi
+    if [ "$logfile" == "" ]
+    then
+        log "logfile not found."
+        log "do you want to show latest logs? [y/N]"
+        read -r -n 1 yn
+        echo ""
+        if [[ "$yn" =~ [yY] ]]
+        then
+            show_latest_logs
+            return
+        fi
+        exit
+    fi
+    if [ ! -f "$logfile" ]
+    then
+        err "logfile not found '$logfile'"
+        exit
+    fi
+    cat "$logfile"
+}
