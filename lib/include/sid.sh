81b0471eca006030fd72d4c1fa4c8772d361abcc
ChillerDragon <ChillerDragon@gmail.com>
2020-02-13 17:27:16 +0000

    Move serverid file to lib/var

--- lib/include/sid.sh
+++ lib/include/sid.sh
@@ -2,10 +2,11 @@
 
 # each server directory has its own server id
 # those are openssl generated base64 encoded
-# and they are generated once and then stored into lib/.server_id
+# and they are generated once and then stored into lib/var/.server_id
 # they are used to kill the process and make sure not to kill the wrong server
 
-SID_FILE=lib/.server_id
+SID_FILE=lib/var/.server_id
+OLD_SID_FILE=lib/.server_id # backwards compability
 
 function generate_sid() {
     server_id=$(openssl rand -hex 16)
@@ -16,8 +17,14 @@ function generate_sid() {
 function get_sid() {
     if [ ! -f "$SID_FILE" ]
     then
-        generate_sid
-        return
+        if [ -f "$OLD_SID_FILE" ]
+        then
+            log "detected old sid file move to new position ..."
+            mv "$OLD_SID_FILE" "$SID_FILE"
+        else
+            generate_sid
+            return
+        fi
     fi
     server_id="$(cat "$SID_FILE")"
     if [ "$server_id" == "" ]
