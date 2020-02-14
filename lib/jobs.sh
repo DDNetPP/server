d12fdfa1deb9c86bc58ce4df5c0f33b6be080aeb
ChillerDragon <ChillerDragon@gmail.com>
2020-02-14 00:08:40 +0000

    Add start|stop|status|interact to jobs

--- lib/jobs.sh
+++ lib/jobs.sh
@@ -11,16 +11,23 @@ source lib/lib.sh
 
 if [ "$#" == "0" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$1" == "help" ]
 then
+    # TODO: add optional global default local argument to list
+    # global would go through all server dirs and list there as well
+    # echo "usage: $0 {create|list} {global|local} <job>"
     echo "usage: $0 create {global|local} <job>"
-    echo "usage: $0 {status|restart|stop|start|run|edit} <job>"
-    echo "usage: $0 [list|kill_all|help]"
+    echo "usage: $0 {status|restart|stop|start|interact|i|run|edit} <job>"
+    echo "usage: $0 [list|kill_all|fix|help]"
     echo "parameters:"
     echo "  status      show status of background job"
     echo "  restart     restart background job"
     echo "  stop        stop background job"
     echo "  start       start background job"
+    echo "  interact    pull background job to foreground"
+    echo "  i           same as interact"
     echo "  run         run foreground (for tests and own background tools)"
     echo "  edit        edit job file"
+    echo "  list        list all jobs off and on"
+    echo "  fix         trys to repair a broken state" # TODO: check all if running and if not move to off dir
     exit
 fi
 
@@ -80,6 +87,9 @@ jobs_dir="$JOB_PATH/$scope"
 jobs_dir_on="$JOB_PATH/$scope/on"
 jobs_dir_off="$JOB_PATH/$scope/off"
 job="$jobs_dir_on/$jobname"
+job_off="$jobs_dir_off/$jobname"
+job_on="$jobs_dir_on/$jobname"
+job_id="job-$scope-$jobname-$server_id"
 if [ ! -f "$job" ]
 then
     job="$jobs_dir_off/$jobname"
@@ -109,6 +119,32 @@ function edit_job() {
     chmod +x "$job"
 }
 
+function show_status() {
+    # prgep does not find screens
+    # shellcheck disable=SC2009
+    proc="$(ps aux | grep "$job_id" | grep -v grep)"
+    if [ "$proc" == "" ]
+    then
+        err "job '$jobname' is not running"
+        exit 1
+    else
+        suc "job '$jobname' is running"
+    fi
+}
+
+function check_kill() {
+    # cleans in fs as on marked jobs that are actually dead
+    if [ -f "$job_on" ]
+    then
+        proc="$(ps aux | grep "$job_id" | grep -v grep)"
+        if [ "$proc" == "" ]
+        then
+            log "cleaning stopped job '$jobname' ..."
+            mv "$job_on" "$job_off" || { err "Error: failed to cleanly stop job '$jobname'"; exit 1; }
+        fi
+    fi
+}
+
 if [ "$mode" == "list" ]
 then
     if [ ! -d "$jobs_dir" ]
@@ -120,15 +156,54 @@ then
 elif [ "$mode" == "edit" ] || [ "$mode" == "create" ]
 then
     edit_job
-elif [ "$mode" == "run" ]
+elif [ "$mode" == "status" ]
 then
-    if is_job "$jobname"
+    show_status
+elif [ "$mode" == "stop" ]
+then
+    proc="$(ps aux | grep "$job_id" | grep -v grep)"
+    if [ "$proc" != "" ]
     then
-        "$job"
+        screen -r "$job_id" -X quit
     else
-        err "job '$jobname' not found"
+        wrn "job '$jobname' is already off"
+    fi
+    if [ -f "$job_on" ]
+    then
+        suc "stopped job '$jobname'"
+        mv "$job_on" "$job_off" || { err "Error: failed to cleanly stop job '$jobname'"; exit 1; }
+    fi
+elif [ "$mode" == "start" ]
+then
+    # prgep does not find screens
+    # shellcheck disable=SC2009
+    proc="$(ps aux | grep "$job_id" | grep -v grep)"
+    if [ "$proc" != "" ]
+    then
+        err "Error: job '$jobname' is already running:"
+        echo "$proc"
         exit 1
     fi
+    if [ ! -f "$job_off" ]
+    then
+        err "Critical error: job '$jobname' is not off"
+        err "Your system seems to be in a erronous state"
+        exit 1
+    fi
+    mv "$job_off" "$job_on" || { err "Error: failed to start job '$jobname'"; exit 1; }
+    log "starting job '$jobname' ..."
+    # echo "job_id=$job_id"
+    # echo "job=$job"
+    screen -AmdS "$job_id" "$job_on"
+    show_status
+elif [ "$mode" == "interact" ] || [ "$mode" == "i" ]
+then
+    show_status
+    screen -r "$job_id"
+    check_kill
+elif [ "$mode" == "run" ]
+then
+    "$job"
 else
     err "invalid argument"
     exit 1
