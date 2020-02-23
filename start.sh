39b2a3805e5941f5dbde9740a2532b7240acca10
ChillerDragon <ChillerDragon@gmail.com>
2020-02-23 17:54:03 +0000

    Add optional map test arg to start.sh (closed #24)

--- start.sh
+++ start.sh
@@ -12,9 +12,57 @@ source lib/lib.sh
 check_deps
 check_running
 
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]
+then
+    echo "usage: ./start.sh [map url]"
+    echo ""
+    echo "when no arguemnt provided a production server is started"
+    echo "it is writing a logfile and running in the background"
+    echo ""
+    echo "when the map argument is provided a test server is started"
+    echo "the map will be downloaded into maps/tmp/mapname.map"
+    echo "and the server will be started with that map and running in the foreground"
+    echo "no logfile will be written"
+    exit 0
+elif [ "$#" -gt 0 ]
+then
+    mapurl="$1"
+    if ! [[ "$mapurl" =~ (http|www).*\.map ]]
+    then
+        err "invalid url '$mapurl'"
+        exit 1
+    fi
+    if [ ! -d maps/ ]
+    then
+        err "maps directory not found."
+        exit 1
+    fi
+    mkdir -p maps/tmp
+    cd maps/tmp
+    mapname="${mapurl##*/}"
+    if ! [[ "$mapname" =~ .map$ ]]
+    then
+        err "Failed to parse mapname '$mapname'"
+        exit 1
+    fi
+    if [ -f "$mapname" ]
+    then
+        rm "$mapname"
+    fi
+    mapname="$(basename "$mapname" .map)"
+    log "downloading map '$mapname' ..."
+    wget "$mapurl"
+    cd ../../
+    read -rp 'password: ' pass
+    log "starting server with password '$pass' ..."
+    ./${srv}_srv_d "sv_map tmp/$mapname;password $pass"
+    exit 0
+fi
+
 logfile="$gitpath_log/$srv_name/logs/${srv_name}_$(date +%F_%H-%M-%S).log"
 cache_logpath "$logfile"
 
 nohup ./${srv}_srv_d "#sid:$server_id" > "$logfile" 2>&1 &
 
 show_logs
+
