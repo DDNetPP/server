5f6c3acb4098accd656936f125d1dc0a83c19a3f
ChillerDragon <ChillerDragon@gmail.com>
2020-05-03 16:02:19 +0200

    Make fddr script actually usable

--- lib/fddr-parse-accounts.sh
+++ lib/fddr-parse-accounts.sh
@@ -3,6 +3,7 @@
 # https://github.com/fokkonaut/F-DDrace/blob/3a41550df2502ca304d79465706e6fd52b951b5f/src/game/server/gamecontext.cpp#L3826-L3862
 
 FDDR_PURGE_FILE=/tmp/fddr-purge.txt
+FDDR_ACC_PATH=./accounts
 
 function fddr.parse_account() {
     account=$1
@@ -15,39 +16,39 @@ function fddr.parse_account() {
     while IFS= read -r line
     do
         if [ "$linenum" == "0" ]; then
-            port="$line"
+            acc_port="$line"
         elif [ "$linenum" == "1" ]; then
-            logged_in="$line"
+            acc_logged_in="$line"
         elif [ "$linenum" == "2" ]; then
-            disabled="$line"
+            acc_disabled="$line"
         elif [ "$linenum" == "3" ]; then
-            password="$line"
+            acc_password="$line"
         elif [ "$linenum" == "4" ]; then
-            username="$line"
+            acc_username="$line"
         elif [ "$linenum" == "5" ]; then
-            client_id="$line"
+            acc_client_id="$line"
         elif [ "$linenum" == "6" ]; then
-            level="$line"
+            acc_level="$line"
         elif [ "$linenum" == "7" ]; then
-            xp="$line"
+            acc_xp="$line"
         elif [ "$linenum" == "8" ]; then
-            money="$line"
+            acc_money="$line"
         elif [ "$linenum" == "9" ]; then
-            kills="$line"
+            acc_kills="$line"
         elif [ "$linenum" == "10" ]; then
-            deaths="$line"
+            acc_deaths="$line"
         elif [ "$linenum" == "11" ]; then
-            police="$line"
+            acc_police="$line"
         elif [ "$linenum" == "12" ]; then
-            survival_kills="$line"
+            acc_survival_kills="$line"
         elif [ "$linenum" == "13" ]; then
-            survival_wins="$line"
+            acc_survival_wins="$line"
         elif [ "$linenum" == "14" ]; then
-            spooky_ghost="$line"
+            acc_spooky_ghost="$line"
         elif [ "$linenum" == "15" ]; then
-            money0="$line"
+            acc_money0="$line"
         elif [ "$linenum" == "16" ]; then
-            money1="$line"
+            acc_money1="$line"
         fi
         linenum="$((linenum+1))"
     done < "$account"
@@ -56,22 +57,31 @@ function fddr.parse_account() {
 function fddr.print_account() {
     path="$1"
     fddr.parse_account "$path"
-    echo "[ === '$username' === ]"
+    echo "[ === '$acc_username' === ]"
     echo "file: $path"
-    echo "password: $password"
-    echo "level: $level xp: $xp"
-    echo "money: $money"
-    echo "$money0"
-    echo "$money1"
-    echo "kills: $kills deaths: $deaths"
+    echo "password: $acc_password"
+    echo "port: $acc_port loggedin: $acc_logged_in"
+    echo "clientID: $acc_client_id disabled: $acc_disabled"
+    echo "level: $acc_level xp: $acc_xp"
+    echo "money: $acc_money police: $acc_police"
+    echo "spooky ghost: $acc_spooky_ghost"
+    echo "$acc_money0"
+    echo "$acc_money1"
+    echo "kills: $acc_kills deaths: $acc_deaths"
+    echo "survival kills=$acc_survival_kills wins=$acc_survival_wins"
 }
 
 function fddr.read_database() {
     :>"$FDDR_PURGE_FILE"
-    for acc in ./accounts/*.acc
+    if [ ! -d "$FDDR_ACC_PATH" ]
+    then
+        echo "Error: '$FDDR_ACC_PATH' is not a directory"
+        exit 1
+    fi
+    for acc in "$FDDR_ACC_PATH"/*.acc
     do
         fddr.parse_account "$acc" || exit 1
-        if [[ "$money" == "0" ]] && [[ "$xp" == "0" ]]
+        if [[ "$acc_money" == "0" ]] && [[ "$acc_xp" == "0" ]]
         then
             echo "$acc" >> "$FDDR_PURGE_FILE"
             fddr.print_account "$acc"
@@ -83,9 +93,17 @@ function fddr.read_purgefile() {
     while IFS= read -r line
     do
         fddr.print_account "$line"
-    done < "$FDDRtPURGE_FILE"
+    done < "$FDDR_PURGE_FILE"
 }
 
-fdrr.read_database
-fdrr.read_purgefile
+if [ "$#" == "0" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]
+then
+    echo "Usage: $(basename "$0") <account>"
+    exit 0
+elif [ "$2" != "" ]
+then
+    FDDR_ACC_PATH="$2"
+fi
+
+fddr.parse_account "$1"
 
