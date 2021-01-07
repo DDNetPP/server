11517701a4daace2c60f584a7e323b56854a5d42
ChillerDragon <ChillerDragon@gmail.com>
2021-01-07 14:41:42 +0000

    Refactor stop script to make sure it kills TEM

--- stop.sh
+++ stop.sh
@@ -15,11 +15,32 @@ then
     rm lib/tmp/logfile.txt
 fi
 
+function kill_by_name() {
+    local proc_name=$1
+    if ! show_procs_name "$proc_name"
+    then
+        return
+    fi
+    if [ "$CFG_SERVER_TYPE" != "tem" ]
+    then
+        log "other processes containing '$proc_name' were found."
+        log "you can stop them all."
+        log "but keep it mind it could stop any application on your server not only tw server"
+    fi
+    log "do you want to stop them all? [y/N]"
+    read -r -n 1 yn
+    echo ""
+    if [[ "$yn" =~ [yY] ]]
+    then
+        pkill -f "$proc_name"
+        log "stopped all processes."
+    else
+        log "aborting stop script."
+    fi
+
+}
+
 proc_str="./$CFG_BIN"
-if [ "$CFG_SERVER_TYPE" == "tem" ]
-then
-    proc_str="settings=$CFG_TEM_SETTINGS"
-fi
 
 if proc_in_screen "$server_id"
 then
@@ -43,23 +64,10 @@ else
 fi
 
 sleep 0.5 # give server time to shutdown
-if show_procs_name "$proc_str"
+
+kill_by_name "$proc_str"
+if [ "$CFG_SERVER_TYPE" == "tem" ]
 then
-    if [ "$CFG_SERVER_TYPE" != "tem" ]
-    then
-        log "other processes containing '$CFG_BIN' were found."
-        log "you can stop them all."
-        log "but keep it mind it could stop any application on your server not only tw server"
-    fi
-    log "do you want to stop them all? [y/N]"
-    read -r -n 1 yn
-    echo ""
-    if [[ "$yn" =~ [yY] ]]
-    then
-        pkill -f "$proc_str"
-        log "stopped all processes."
-    else
-        log "aborting stop script."
-    fi
+    kill_by_name "settings=$CFG_TEM_SETTINGS"
 fi
 
