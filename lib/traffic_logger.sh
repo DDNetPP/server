d432ac52326f94851d3259f43fc6dcef8ae0c053
ChillerDragon <ChillerDragon@gmail.com>
2021-07-07 13:08:20 +0200

    Cache ip and port and use external service for ip

--- lib/traffic_logger.sh
+++ lib/traffic_logger.sh
@@ -17,6 +17,23 @@ DDOS_TRESHOLD=6
 
 current_interval=0
 
+server_ip="$(get_tw_config bindaddr 127.0.0.1)"
+server_port="$(get_tw_config sv_port 8303)"
+if [ "$server_ip" == "127.0.0.1" ]
+then
+	if [ ! "$(command -v curl)" ]
+	then
+		err "Error: command $(tput bold)curl$(tput sgr0) not found"
+		err "	    could not determine ip address"
+	else
+		server_ip="$(curl https://ipinfo.io/ip)"
+	fi
+fi
+if [ "$server_ip" == "" ]
+then
+	err "could not determine ip address"
+fi
+
 mkdir -p logs/ddos
 
 function log_ddos() {
@@ -24,28 +41,13 @@ function log_ddos() {
 	local lines
 	local players
 	local ddos_log
-	local server_ip
-	local server_port
 	logfile="$1"
 	if [ ! -f "$logfile" ]
 	then
 		return
 	fi
-	server_ip="$(get_tw_config bindaddr 127.0.0.1)"
-	server_port="$(get_tw_config sv_port 8303)"
-	if [ "$server_ip" == "127.0.0.1" ]
-	then
-		if [ ! "$(command -v hostname)" ]
-		then
-			err "Error: command $(tput bold)hostname$(tput sgr0) not found"
-			err "	    could not determine ip address"
-			return
-		fi
-		server_ip="$(hostname -i)"
-	fi
 	if [ "$server_ip" == "" ]
 	then
-		err "could not determine ip address"
 		return
 	fi
 	lines="$(sed '1,/^KEYWORD$/d' "$logfile" | wc -l)"
