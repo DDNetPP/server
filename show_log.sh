028bb376f32569885de07742ad3ef467139f3434
ChillerDragon <ChillerDragon@gmail.com>
2021-07-11 12:28:19 +0200

    Spaces to tabs in show_log.sh

--- show_log.sh
+++ show_log.sh
@@ -2,9 +2,9 @@
 
 if [ ! -f lib/lib.sh ]
 then
-    echo "Error: lib/lib.sh not found!"
-    echo "make sure you are in the root of the server repo"
-    exit 1
+	echo "Error: lib/lib.sh not found!"
+	echo "make sure you are in the root of the server repo"
+	exit 1
 fi
 
 source lib/lib.sh
@@ -14,21 +14,21 @@ follow=""
 
 for arg in "$@"
 do
-    if [ "$arg" == "--help" ] || [ "$1" == "-h" ]
-    then
-        echo "usage: ./show_log.sh [logname] [-f] [logname]"
-        echo "options:"
-        echo "  -h          show this help"
-        echo "  -f          follows log using tail -f"
-        echo "  logname     directory in ./logs/logname for local log types"
-        exit 0
-    elif [ "$arg" == "-f" ]
-    then
-        follow=-f
-    elif [ "$logpath" == "" ]
-    then
-        logpath="$arg"
-    fi
+	if [ "$arg" == "--help" ] || [ "$1" == "-h" ]
+	then
+		echo "usage: ./show_log.sh [logname] [-f] [logname]"
+		echo "options:"
+		echo "  -h          show this help"
+		echo "  -f          follows log using tail -f"
+		echo "  logname     directory in ./logs/logname for local log types"
+		exit 0
+	elif [ "$arg" == "-f" ]
+	then
+		follow=-f
+	elif [ "$logpath" == "" ]
+	then
+		logpath="$arg"
+	fi
 done
 
 show_latest_log "$follow" "$logpath"
