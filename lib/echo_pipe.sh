df61a7bd92ca92a9afd16bfc93c7e9c26dae162a
ChillerDragon <ChillerDragon@gmail.com>
2021-08-30 10:23:07 +0200

    Allow FILE.. for echo pipe

--- lib/echo_pipe.sh
+++ lib/echo_pipe.sh
@@ -5,23 +5,7 @@ function format_line() {
 	echo "echo \"${line//\"/\\\"}\""
 }
 
-if [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$#" -gt "1" ]
-then
-	# TODO:
-	# support [FILE...]
-	echo "usage: $(basename "$0") [FILE]"
-	echo "wraps every line of given file in a echo"
-	echo "so it can be viewed as teeworlds admin"
-	echo "using the rcon command 'exec file.txt'"
-	echo "if no file is given it uses stdin"
-	exit 0
-elif [ "$#" == "0" ]
-then
-	while IFS=$'\n' read -r line;
-	do
-		format_line "$line"
-	done
-else
+function format_file() {
 	file="$1"
 	if [ ! -f "$file" ]
 	then
@@ -32,5 +16,30 @@ else
 	do
 		format_line "$line"
 	done < "$file"
+}
+
+if [ "$#" == "0" ]
+then
+	while IFS=$'\n' read -r line;
+	do
+		format_line "$line"
+	done
+	exit 0
 fi
+
+for arg in "$@"
+do
+	if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]
+	then
+		echo "usage: $(basename "$0") [FILE..]"
+		echo "description:"
+		echo "  wraps every line of given file in a echo"
+		echo "  so it can be viewed as teeworlds admin"
+		echo "  using the rcon command 'exec file.txt'"
+		echo "  if no file is given it uses stdin"
+		exit 0
+	else
+		format_file "$arg"
+	fi
+done
 
--- lib/test/unit/echo_pipe.sh
+++ lib/test/unit/echo_pipe.sh
@@ -9,9 +9,20 @@ fi
 
 source lib/lib.sh
 
-log -n "echo_pipe.sh ... "
+log -n "echo_pipe.sh [stdin] ... "
 
 if ! diff <(./lib/echo_pipe.sh < ./lib/test/unit/data/echo_pipe.in) ./lib/test/unit/data/echo_pipe.out
+then
+	echo "FAIL"
+	err "echo pipe failed"
+	exit 1
+else
+	echo "OK"
+fi
+
+log -n "echo_pipe.sh [multi file] ... "
+
+if ! diff <(./lib/echo_pipe.sh ./lib/test/unit/data/echo_pipe.in ./lib/test/unit/data/echo_pipe.in) <(cat ./lib/test/unit/data/echo_pipe.out ./lib/test/unit/data/echo_pipe.out)
 then
 	echo "FAIL"
 	err "echo pipe failed"
