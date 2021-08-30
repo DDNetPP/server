18980e303eeb9a98338240c805c685f4e94b330f
ChillerDragon <ChillerDragon@gmail.com>
2021-08-30 09:49:07 +0200

    Spaces to tabs in logs.sh

--- lib/include/logs.sh
+++ lib/include/logs.sh
@@ -1,48 +1,48 @@
 #!/bin/bash
 
 function check_logpath() {
-    local logpath
-    if [ ! -d lib/ ] || [ ! -f lib/tmp/logpath.txt ]
-    then
-        return
-    fi
-    logpath="$(cat lib/tmp/logpath.txt)"
-    if [ "$logpath" == "" ]
-    then
-        return
-    fi
-    if [ ! -f "$logpath" ] && [ ! -f "$logpath.txt" ]
-    then
-        wrn "WARNING: did not find logfile:"
-        echo "$logpath"
-    fi
+	local logpath
+	if [ ! -d lib/ ] || [ ! -f lib/tmp/logpath.txt ]
+	then
+		return
+	fi
+	logpath="$(cat lib/tmp/logpath.txt)"
+	if [ "$logpath" == "" ]
+	then
+		return
+	fi
+	if [ ! -f "$logpath" ] && [ ! -f "$logpath.txt" ]
+	then
+		wrn "WARNING: did not find logfile:"
+		echo "$logpath"
+	fi
 }
 
 function cache_logpath() {
-    local logpath="$1"
-    if [ ! -d lib/ ]
-    then
-        return
-    fi
-    mkdir -p lib/tmp
-    if [ "$IS_DUMPS_LOGPATH" == "1" ]
-    then
-        if [ "$HOME" == "" ]
-        then
-            err "Error: \$HOME is not set"
-            exit 1
-        fi
-        dumpsdir="^$HOME/.teeworlds/dumps/"
-        if [[ ! "$logpath" =~ $dumpsdir ]]
-        then
-            logpath="$HOME/.teeworlds/dumps/$logpath"
-        fi
-    fi
-    echo "$logpath" > lib/tmp/logpath.txt
+	local logpath="$1"
+	if [ ! -d lib/ ]
+	then
+		return
+	fi
+	mkdir -p lib/tmp
+	if [ "$IS_DUMPS_LOGPATH" == "1" ]
+	then
+		if [ "$HOME" == "" ]
+		then
+			err "Error: \$HOME is not set"
+			exit 1
+		fi
+		dumpsdir="^$HOME/.teeworlds/dumps/"
+		if [[ ! "$logpath" =~ $dumpsdir ]]
+		then
+			logpath="$HOME/.teeworlds/dumps/$logpath"
+		fi
+	fi
+	echo "$logpath" > lib/tmp/logpath.txt
 }
 
 function get_latest_logfile() {
-    find "$LOGS_PATH_FULL" | sort | tail -n1
+	find "$LOGS_PATH_FULL" | sort | tail -n1
 }
 
 function show_latest_log() {
@@ -91,20 +91,20 @@ function show_latest_log() {
 }
 
 function show_log_file() {
-    local logfile="$1"
-    log "logfile: $logfile"
-    log "do you want to show the logfile? [y/N]"
-    read -r -n 1 yn
-    echo ""
-    if [[ ! "$yn" =~ [yY] ]]
-    then
-        return
-    fi
-    if [ ! -f "$logfile" ] || [ "$logfile" == "" ]
-    then
-        err "logfile not found '$logfile'"
-        exit 1
-    fi
-    cat "$logfile"
+	local logfile="$1"
+	log "logfile: $logfile"
+	log "do you want to show the logfile? [y/N]"
+	read -r -n 1 yn
+	echo ""
+	if [[ ! "$yn" =~ [yY] ]]
+	then
+		return
+	fi
+	if [ ! -f "$logfile" ] || [ "$logfile" == "" ]
+	then
+		err "logfile not found '$logfile'"
+		exit 1
+	fi
+	cat "$logfile"
 }
 
