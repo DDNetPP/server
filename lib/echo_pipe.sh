3574268b586752f789044834aa69c5ea9cf24367
ChillerDragon <ChillerDragon@gmail.com>
2021-08-30 10:13:00 +0200

    Spaces to tabs echo_pipe.sh

--- lib/echo_pipe.sh
+++ lib/echo_pipe.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 function format_line() {
-    local line="$1"
-    echo "echo \"${line//\"/\\\"}\""
+	local line="$1"
+	echo "echo \"${line//\"/\\\"}\""
 }
 
 if [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$#" -gt "1" ]
 then
-    # TODO:
-    # support [FILE...]
-    echo "usage: $(basename "$0") [FILE]"
-    echo "wraps every line of given file in a echo"
-    echo "so it can be viewed as teeworlds admin"
-    echo "using the rcon command 'exec file.txt'"
-    echo "if no file is given it uses stdin"
-    exit 0
+	# TODO:
+	# support [FILE...]
+	echo "usage: $(basename "$0") [FILE]"
+	echo "wraps every line of given file in a echo"
+	echo "so it can be viewed as teeworlds admin"
+	echo "using the rcon command 'exec file.txt'"
+	echo "if no file is given it uses stdin"
+	exit 0
 elif [ "$#" == "0" ]
 then
-    while IFS=$'\n' read -r line;
-    do
-        format_line "$line"
-    done
+	while IFS=$'\n' read -r line;
+	do
+		format_line "$line"
+	done
 else
-    file="$1"
-    if [ ! -f "$file" ]
-    then
-        echo "echo \"Error: file '$file' not found.\""
-        exit 1
-    fi
-    while IFS= read -r line;
-    do
-        format_line "$line"
-    done < "$file"
+	file="$1"
+	if [ ! -f "$file" ]
+	then
+		echo "echo \"Error: file '$file' not found.\""
+		exit 1
+	fi
+	while IFS= read -r line;
+	do
+		format_line "$line"
+	done < "$file"
 fi
 
