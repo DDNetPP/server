a82aea10cf262323a8b14d34710cd47899dcd927
ChillerDragon <ChillerDragon@gmail.com>
2021-08-31 10:40:10 +0200

    Use same tmp/tests dir as integration test

--- lib/test/unit/traffic_logger.sh
+++ lib/test/unit/traffic_logger.sh
@@ -11,11 +11,11 @@ source lib/lib.sh
 
 log -n "traffic_logger.sh [show_known_ips] ... "
 
-mkdir -p ./lib/tmp/test
-cp ./lib/test/unit/data/traffic_raw.txt ./lib/tmp/test/traffic_raw.txt
-show_known_ips ./lib/tmp/test/traffic_raw.txt ./lib/known_ips.txt
+mkdir -p ./lib/tmp/tests
+cp ./lib/test/unit/data/traffic_raw.txt ./lib/tmp/tests/traffic_raw.txt
+show_known_ips ./lib/tmp/tests/traffic_raw.txt ./lib/known_ips.txt
 if ! diff \
-	./lib/tmp/test/traffic_raw.txt \
+	./lib/tmp/tests/traffic_raw.txt \
 	./lib/test/unit/data/traffic_known_ips.txt
 then
 	echo "FAIL"
