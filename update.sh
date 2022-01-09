6000787971356bfcfde9fc3d149c1d6b734617a9
ChillerDragon <ChillerDragon@gmail.com>
2022-01-09 14:52:49 +0100

    Support theme scripts

--- update.sh
+++ update.sh
@@ -62,6 +62,55 @@ do
 	fi
 done
 
+function map_themes_pre() {
+	if [ ! -d "$CFG_GIT_ROOT"/maps-scripts ]
+	then
+		return
+	fi
+	OldMapHashes=()
+	local theme
+	for theme in "$CFG_GIT_ROOT"/maps-scripts/*/
+	do
+		local theme="$(basename "$theme")"
+		if [ -f ./maps/"$theme".map ]
+		then
+			OldMapHashes["$theme"]="$(sha1sum ./maps/"$theme".map | cut -d' ' -f1)"
+		fi
+	done
+}
+function map_themes_post() {
+	if [ ! -d "$CFG_GIT_ROOT"/maps-scripts ]
+	then
+		return
+	fi
+	local theme
+	local t
+	for theme in "$CFG_GIT_ROOT"/maps-scripts/*/
+	do
+		theme="$(basename "$theme")"
+		log "theme $theme"
+		if [ -f ./maps/"$theme".map ]
+		then
+			if [ "${OldMapHashes["$theme"]}" != "$(sha1sum ./maps/"$theme".map | cut -d' ' -f1)" ]
+			then
+				log "map '$theme' updated generating new themes ..."
+				log "  old: ${OldMapHashes["$theme"]}"
+				log "  new: $(sha1sum ./maps/"$theme".map | cut -d' ' -f1)"
+				for t in "$CFG_GIT_ROOT"/maps-scripts/"$theme"/*.py
+				do
+					log "generating '$(basename "$t" .py)' theme for '$theme' ..."
+					mkdir -p ./designs/"$theme"
+					"$t" ./maps/"$theme".map ./designs/"$theme"/"$(basename "$t" .py)".map
+				done
+			else
+				log "skipping non updated map"
+				log "  old: ${OldMapHashes["$theme"]}"
+				log "  new: $(sha1sum ./maps/"$theme".map | cut -d' ' -f1)"
+			fi
+		fi
+	done
+}
+
 if [ "$arg_type" == "bot" ]
 then
 	if [ "$arg_refresh" == "1" ]
@@ -79,7 +128,9 @@ then
 		cmake_refresh_teeworlds_binary
 		exit 0
 	fi
+	map_themes_pre
 	cmake_update_teeworlds "$@"
+	map_themes_post
 	git_save_pull
 elif [ "$CFG_SERVER_TYPE" == "tem" ] || [ "$arg_type" == "tem" ]
 then
