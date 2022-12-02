44f95645488cdcabd68c42b6b64387f86fc204d9
ChillerDragon <ChillerDragon@gmail.com>
2022-12-02 10:17:06 +0100

    Add auto scrolling to interactive console

--- lib/console.sh
+++ lib/console.sh
@@ -11,16 +11,26 @@ source lib/lib.sh
 
 check_server_dir
 
+message='here could be your ad'
+
 logfile="$(get_latest_logfile)"
+last_log_line=''
 
 function render() {
+	local newest_line
+	newest_line="$(tail -n 1 "$logfile")"
+	if [ "$newest_line" == "$last_log_line" ]
+	then
+		return
+	fi
+	last_log_line="$newest_line"
 	log_height="$(tput lines)"
 	log_height="$((log_height - 3))"
 	max_width="$(tput cols)"
 
 	tail -n "$log_height" "$logfile"
 	printf '%0.s-' $(seq 1 "$max_width")
-	echo "you have to manually press enter to refresh the log because devs are drunk"
+	echo "$message"
 	printf '%0.s-' $(seq 1 "$max_width")
 	printf '\n> %s' "$input"
 }
@@ -31,21 +41,23 @@ function send_fifo() {
 
 render
 
+# avoid 'ENTER' presses scrolling
+# this hides all stdin in stdout
+stty -echo
+
 while true
 do
-	tput cup "$((log_height + 2))" 1
+	printf "\33[%d;%dH%s" "$((log_height + 3))" 3 "$input"
 	ifs="$IFS"
-	IFS= read -n 1 -r -p "" inp
+	IFS= read -r -t 0.01 -s -d '' -n1 inp
 	IFS="$ifs"
-	if [ "$inp" == '' ]
+	if [ "$inp" == $'\n' ]
 	then
 		send_fifo "$input"
 		input=''
-	elif [ "$inp" == $'^?' ]
-	then
-		input=''
+	else
+		input+="$inp"
 	fi
-	input+="$inp"
 	render
 done
 
