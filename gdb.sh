636dd5b56b0ac2e3b68a2f89e47691e48fd7caa9
ChillerDragon <ChillerDragon@gmail.com>
2023-02-25 11:00:06 +0100

    Attempt to fix ddnet++ crash SIGPIPE
    https://github.com/DDNetPP/DDNetPP/issues/346

--- gdb.sh
+++ gdb.sh
@@ -18,7 +18,14 @@ archive_gmon
 logfile="$LOGS_PATH_FULL_TW/${CFG_SRV_NAME}_$(date +%F_%H-%M-%S)${CFG_LOG_EXT}"
 cache_logpath "$logfile"
 
-run_cmd="$CFG_ENV_RUNTIME gdb -ex=run --args ./\"$CFG_BIN\" \"logfile $logfile;#sid:$SERVER_UUID\""
+# https://stackoverflow.com/questions/18935446/program-received-signal-sigpipe-broken-pipe#18963142
+# https://github.com/ddnet/ddnet/pull/3527
+# we need "handle SIGPIPE nostop noprint pass"
+# when running ddnet based servers because it uses curl
+# and curl throws a sigpipe
+# the ddnet code catches that but gdb catches it first and then crashes the server
+# TODO: make this a gdb options config or use a gdbrc file and also include that in loop_gdb
+run_cmd="$CFG_ENV_RUNTIME gdb -ex='handle SIGPIPE nostop noprint pass' -ex=run --args ./\"$CFG_BIN\" \"logfile $logfile;#sid:$SERVER_UUID\""
 log "$run_cmd"
 eval "$run_cmd"
 
