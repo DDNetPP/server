d97c93a7ac5e817b5452c086c3eb5368af4fb00d
ChillerDragon <ChillerDragon@gmail.com>
2023-05-29 13:22:10 +0200

    Better error on git pull

--- lib/include/git.sh
+++ lib/include/git.sh
@@ -4,7 +4,7 @@ function git_save_pull() {
     if [ "$(git status | tail -n1)" != "nothing to commit, working tree clean" ]
     then
         wrn "WARNING: git pull failed! Is your working tree clean?"
-        wrn "  git status"
+        wrn "  cd $PWD && git status"
         return
     fi
     git pull
