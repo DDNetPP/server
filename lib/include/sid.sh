96698481af757c1ceef0d877c7cf9e68fc21a41b
ChillerDragon <ChillerDragon@gmail.com>
2024-07-24 11:56:57 +0800

    Fix pwd dependenet SID path

--- lib/include/sid.sh
+++ lib/include/sid.sh
@@ -4,8 +4,8 @@
 # and they are generated once and then stored into lib/var/.server_id
 # they are used to kill the process and make sure not to kill the wrong server
 
-SID_FILE=lib/var/.server_id
-OLD_SID_FILE=lib/.server_id # backwards compability
+SID_FILE="$SCRIPT_ROOT/lib/var/.server_id"
+OLD_SID_FILE="$SCRIPT_ROOT/lib/.server_id" # backwards compability
 
 function generate_uuid() {
 	od -xN16 /dev/urandom | awk -v OFS=- '{print $2$3,$4,$5,$6,$7$8$9; exit}'
