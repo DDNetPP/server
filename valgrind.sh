67bca61e17ac17e0580c0152c3bf8659d70cdc98
ChillerDragon <ChillerDragon@gmail.com>
2025-11-29 19:49:27 +0100

    I continue this another day i guess

--- valgrind.sh
+++ valgrind.sh
@@ -6,20 +6,21 @@
 #          but last time i lost my untracked valgrind.sh so this time i rather track
 #          the messy one than to start from scratch again
 
-# tools:
-# memcheck
-# massif
-
 srv_commit=77931f936c
 SRV_BIN=./bin/solofng1_no_asan_"${srv_commit}"
 
 cp ~/git/ddnet-insta/build-valgrind/DDNet-Server "$SRV_BIN" || exit 1
 
+
+# valgrind \
+# 	--tool=memcheck \
+# 	--gen-suppressions=all \
+# 	--suppressions=./lib/supp/memcheck.supp \
+# 	--leak-check=full \
+# 	--show-leak-kinds=all \
+
 valgrind \
 	--tool=massif \
-	--gen-suppressions=all \
 	--suppressions=./lib/supp/memcheck.supp \
-	--leak-check=full \
-	--show-leak-kinds=all \
-	"$SRV_BIN" &> valgrind_"$(date '+%F_%H-%M')".txt
+	"$SRV_BIN" &> logs/valgrind_"$(date '+%F_%H-%M')".txt
 
