b45870fd60a5190d775364e2e5b32b0a3e6fa70a
ChillerDragon <ChillerDragon@gmail.com>
2025-11-29 19:42:37 +0100

    Add hardcodet valgrind.sh

---
+++ valgrind.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# WARNING: this script is not really ready to be used yet
+#          valgrind cant run with asan so the entrie setup is a bit messy
+#          not sure if i ever put enough love into this so its actually usable
+#          but last time i lost my untracked valgrind.sh so this time i rather track
+#          the messy one than to start from scratch again
+
+# tools:
+# memcheck
+# massif
+
+srv_commit=77931f936c
+SRV_BIN=./bin/solofng1_no_asan_"${srv_commit}"
+
+cp ~/git/ddnet-insta/build-valgrind/DDNet-Server "$SRV_BIN" || exit 1
+
+valgrind \
+	--tool=massif \
+	--gen-suppressions=all \
+	--suppressions=./lib/supp/memcheck.supp \
+	--leak-check=full \
+	--show-leak-kinds=all \
+	"$SRV_BIN" &> valgrind_"$(date '+%F_%H-%M')".txt
+
