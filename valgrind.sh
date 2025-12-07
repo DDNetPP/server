91275cc324ef0aa91247f71d9ec88d6137df4767
ChillerDragon <chillerdragon@gmail.com>
2025-12-07 09:13:10 +0100

    valgrind check build env

--- valgrind.sh
+++ valgrind.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 
+if [ ! -f lib/lib.sh ]
+then
+    echo "Error: lib/lib.sh not found!"
+    echo "make sure you are in the root of the server repo"
+    exit 1
+fi
+
+source lib/lib.sh
+
+is_asan_runtime() {
+	if printf '%s' "$CFG_ENV_RUNTIME" | grep -qE '(env_san.sh|fsanitize)'
+	then
+		return 0
+	fi
+	return 1
+}
+
+is_asan_build() {
+	if printf '%s' "$CFG_ENV_BUILD" | grep -qE '(env_san.sh|fsanitize)'
+	then
+		return 0
+	fi
+	return 1
+}
+
+if is_asan_runtime || is_asan_build
+then
+	err "Error: in your server.cnf you enabled a sanitizer already"
+	err "       valgrind does not work well when combined with asan and ubsan"
+	err "       go to your server.cnf and check where you set env_build and env_runtime"
+	err "       their current values are:"
+	err ""
+	err "        env_build=$CFG_ENV_BUILD"
+	err "        env_runtime=$CFG_ENV_RUNTIME"
+	err ""
+	exit 1
+fi
+
+
 # WARNING: this script is not really ready to be used yet
 #          valgrind cant run with asan so the entrie setup is a bit messy
 #          not sure if i ever put enough love into this so its actually usable
